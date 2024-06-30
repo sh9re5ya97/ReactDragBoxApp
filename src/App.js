4cf31342266f57dbddfa099d082392c645933ecd
Shreya Singal <singalshreya97@gmail.com>
2024-06-30 08:53:40 +0530

    draggable element and addparent

--- src/App.js
+++ src/App.js
@@ -1,13 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef } from 'react';
 import DraggableBox from './Components/DraggableBox';
 
-function App() {
+const App = () => {
+  const [layers, setLayers] = useState(1);
+  const draggableRefs = useRef([]);
+
+  const addParent = () => {
+    setLayers(layers + 1);
+  };
+
+  const renderNestedBoxes = (layer) => {
+    if (layer === 0) {
+      return (
+        <div
+          style={{
+            width: 100+layer*10+'px',
+            height: 100+layer*10+'px',
+            backgroundColor: 'blue',
+            position: 'relative',
+            cursor: 'grab',
+          }}
+        />
+      );
+    }
+    return (
+      <DraggableBox ref={(el) => (draggableRefs.current[layer] = el)}>
+        <div
+          style={{
+            width:  100+layer*10+'px',
+            height:  100+layer*10+'px',
+            position: 'relative',
+            border: '1px solid black',
+          }}
+        >
+          {renderNestedBoxes(layer - 1)}
+        </div>
+      </DraggableBox>
+    );
+  };
+
   return (
-    <div className="App">
-     <DraggableBox />
+    <div>
+      <button onClick={addParent}>Add Parent</button>
+      <div
+        style={{
+          width: '100%',
+          height: '100vh',
+          position: 'relative',
+          border: '1px solid black',
+          overflow: 'hidden',
+          margin: '20px 0',
+        }}
+      >
+        {renderNestedBoxes(layers)}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
--- src/Components/DraggableBox.js
+++ src/Components/DraggableBox.js
@@ -1,19 +1,25 @@
-import React from 'react';
+// src/components/DraggableBox.js
+
+import React, { forwardRef } from 'react';
 import Draggable from 'react-draggable';
 
-const DraggableBox = () => {
+const DraggableBox = forwardRef(({ children }, ref) => {
   return (
-    <Draggable>
+    <Draggable bounds="parent">
       <div
+        ref={ref}
         style={{
           width: '100px',
           height: '100px',
-          backgroundColor: 'blue',
+          backgroundColor: 'rgba(0, 0, 255, 0.1)',
           cursor: 'grab',
+          position: 'relative',
         }}
-      />
+      >
+        {children}
+      </div>
     </Draggable>
   );
-};
+});
 
 export default DraggableBox;
